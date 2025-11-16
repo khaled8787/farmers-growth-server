88667d1a1b0ffb93f9d23a17b769eb1eeaddc298
khaled8787 <khaledmahmud01737@gmail.com>
2025-11-16 11:20:44 +0600

    token problem solved

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log(" MongoDB connected"))
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/crops", cropsRouter);
